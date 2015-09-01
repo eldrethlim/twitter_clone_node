b50f70bd3cea4fa4c2466d5310b36f8eae816a23
eldrethlim <eldrethlim@gmail.com>
2015-09-02 06:08:52 +0800

    remove commented code

--- index.js
+++ index.js
@@ -9,15 +9,3 @@ require('./router')(app)
 var server = app.listen(config.get('server:port'), config.get('server:host'))
 
 module.exports = server
-//
-// var _ = require('lodash')
-//   , express = require('express')
-//   , app = express()
-//   , config = require('./config')
-//
-// require('./middleware')(app)
-// require('./router')(app)
-//
-// var server = app.listen(config.get('server:port'), config.get('server:host'))
-//
-// module.exports = server
